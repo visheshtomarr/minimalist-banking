209bb7fd7a85c9c05778d0628583cb8cd9379916
visheshtomarr <visheshtomar7800@gmail.com>
2025-02-15 22:16:01 +0530

    Refactored code to format dates

--- script.js
+++ script.js
@@ -40,14 +40,14 @@ const account5 = {
   pin: 5555,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-11-18T21:31:17.178Z',
+    '2023-12-23T07:42:02.383Z',
+    '2024-01-28T09:15:04.904Z',
+    '2024-04-01T10:17:24.185Z',
+    '2024-05-08T14:11:59.604Z',
+    '2025-02-10T17:01:17.194Z',
+    '2025-02-11T23:36:17.929Z',
+    '2025-02-12T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -60,14 +60,14 @@ const account6 = {
   pin: 6666,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2023-11-01T13:15:33.035Z',
+    '2023-11-30T09:48:16.867Z',
+    '2024-12-25T06:04:23.907Z',
+    '2024-01-25T14:18:46.235Z',
+    '2024-02-05T16:33:06.386Z',
+    '2024-04-10T14:43:26.374Z',
+    '2025-01-25T18:49:59.371Z',
+    '2025-01-26T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -109,6 +109,24 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
+// Format date and return the current movement date.
+const formatMovementDate = date => {
+  // Function to calculate days passed according to the current date.
+  const calcDaysPassed = (date1, date2) => 
+    Math.round(Math.abs(date2 - date1) / (24 * 60 * 60 * 1000))
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  if (daysPassed === 0) return 'Today'; 
+  if (daysPassed === 1) return 'Yesterday'; 
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+}
+
 // Function to display the movements.
 const displayMovements = (account, sort = false) => {
   // Empty the current 'movements' container.
@@ -123,11 +141,8 @@ const displayMovements = (account, sort = false) => {
     const type = movement > 0 ? 'deposit' : 'withdrawal';
 
     if (account.movementsDates) {
-      const date = new Date(account.movementsDates[i] || '');
-      const day = `${date.getDate()}`.padStart(2, 0);
-      const month = `${date.getMonth() + 1}`.padStart(2, 0);
-      const year = date.getFullYear();
-      const displayDate = `${day}/${month}/${year}`;
+      const date = new Date(account.movementsDates[i]);
+      const displayDate = formatMovementDate(date);
       
       const html = `
         <div class="movements__row">
